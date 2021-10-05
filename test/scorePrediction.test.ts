9e594173d6932417b473ff6411e3fa45e9843712
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2021-10-05 20:07:51 +0530

    added 3rd test case

--- test/scorePrediction.test.ts
+++ test/scorePrediction.test.ts
@@ -15,3 +15,12 @@ test("predicting score ,which will be between 1 to 6", () => {
   let recieved = predictScore(batsMan);
   expect(recieved).toBeGreaterThanOrEqual(expected);
 });
+describe("spying for score prediction ", () => {
+  test("predicting score for Kirat Nohil,which is 6", () => {
+    jest.spyOn(global.Math, "random").mockImplementation(() => 0.6);
+    let expected = 2;
+    let batsMan = "Kirat_Nohil";
+    let recieved = predictScore(batsMan);
+    expect(recieved).toBe(expected);
+  });
+});
