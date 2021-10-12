5f2cfe481e615bc612da77470973057e3891a703
ManikantaKodimala <manikanta.kodimala@everest.engineering>
2021-10-12 20:20:02 +0530

    added test case for team score card

--- test/team.test.ts
+++ test/team.test.ts
@@ -37,11 +37,19 @@ describe("tesing Team class and funtionaities", () => {
   });
 
   test("checks whether player is out or not,here player is out ", () => {
-    playerName = "Kirat_Nohil";
+    playerName = "R_Rumrah";
     score = -1;
     team.setScore(playerName, score);
     let excpeted = true;
     let recieved = team.isPlayerOut(playerName);
+    expect(recieved).toBe(excpeted);
+  });
+
+  test("checks whether player has palyed or not,here player has not played ", () => {
+    playerName = "Kirat_Nohil";
+    let excpeted = true;
+    team.setPlayer(playerName);
+    let recieved = team.isPlayerPlayed(playerName);
     expect(recieved).toBe(excpeted);
   });
 
@@ -57,14 +65,12 @@ describe("tesing Team class and funtionaities", () => {
     score = -1;
     team.setScore(playerName, score);
     let excpeted = true;
-    let recieved = team.isPlayerPlayed(playerName);
+    let recieved = team.isPlayerOut(playerName);
     expect(recieved).toBe(excpeted);
   });
 
   test("checks numbers of balls a player played,here zero", () => {
     playerName = "Kirat_Nohil";
-    score = 0;
-    team.setScore(playerName, score);
     let excpeted = 0;
     let recieved = team.getBallsPlayedBy(playerName);
     expect(recieved).toBe(excpeted);
@@ -77,5 +83,18 @@ describe("tesing Team class and funtionaities", () => {
     let excpeted = 1;
     let recieved = team.getBallsPlayedBy(playerName);
     expect(recieved).toBe(excpeted);
+  });
+
+  test("check the team score card who has played,now it will return empty string", () => {
+    let expected = "";
+    let recieved = team.TeamScoreCard();
+    expect(recieved).toEqual(expected);
+  });
+
+  test("check the team score card who has played,now it will return string of players score along with balls played", () => {
+    team.setPlayer("Kirat_Nohil");
+    let expected = "Kirat_Nohil - 0* (0 balls)\n";
+    let recieved = team.TeamScoreCard();
+    expect(recieved).toEqual(expected);
   });
 });
